b58241bdfc2342af6507cb794b83d7ec11c72a24
Antonella Ciceri <antociceri@gmail.com>
2021-06-23 21:29:17 -0300

    formulario/profesional

--- src/main/java/com/kinesysApp/kinesys/servicios/ProfesionalServicio.java
+++ src/main/java/com/kinesysApp/kinesys/servicios/ProfesionalServicio.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class ProfesionalServicio {
+public class  ProfesionalServicio {
 
     @Autowired
     private ProfesionalRepositorio profesionalRepositorio;
